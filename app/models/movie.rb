43ab8fd6a9e297b2d1d7eb2c4810005ccfa9423a
sagarshah16 <sagar@Class2018s-MBP.home>
2016-02-09 16:47:43 -0500

    asstes setting

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,8 +2,10 @@ class Movie < ActiveRecord::Base
 	searchkick
 	belongs_to :user
     has_many :reviews
-  has_attached_file :image, styles: { medium: "400x600#"}
+ 
+has_attached_file :image, styles: { medium: "400x600#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
 #validates_attachment_file_name :image, matches: {matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]}
   # , default_url: "/images/:style/missing.png" // when image is missing
   #, "image/jpeg", "image/png", "image/gif"
