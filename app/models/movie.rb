105c82951feb0baf75915137b6032dc581b3f76f
sagarshah16 <sagar@Class2018s-MBP.home>
2016-02-09 15:44:58 -0500

    changes sql

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,7 +2,7 @@ class Movie < ActiveRecord::Base
 	searchkick
 	belongs_to :user
     has_many :reviews
-  has_attached_file :image, styles: { medium: "400x600"}
+  has_attached_file :image, styles: { medium: "400x600#"}
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 #validates_attachment_file_name :image, matches: {matches: [/png\Z/, /jpe?g\Z/, /gif\Z/]}
   # , default_url: "/images/:style/missing.png" // when image is missing
