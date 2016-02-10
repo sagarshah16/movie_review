1d4bcce7c07bf816cc291804fe60e5d194985743
sagarshah16 <sagar@Class2018s-MBP.home>
2016-02-10 15:50:49 -0500

    added line for Devise

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,7 @@ module MovieReview
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.serve_static_files = true
+
+    config.assets.initiaize_on_precompile = false
   end
 end
